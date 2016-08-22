02e3313091db65c896ea5ac2049201edb5e479ef
Fiahil <fiahil@gmail.com>
2016-08-23 00:06:13 +0200

    add new model in producer

--- scanner/app.py
+++ scanner/app.py
@@ -7,34 +7,41 @@ from datetime import timedelta
 import time
 import json
 import random
+import base64
 
-pokemons = {
-    1: 'Salameche',
-    2: 'Bulbizarre',
-    3: 'Pikachu',
-    4: 'Ponita'
-}
+encounters = {}
+cycle_size = 2
+
+def scan():
+    global encounters
+    t = datetime.now()
+
+    if t.second == 0 and t.minute % cycle_size:
+        encounters = {}
+
+    encounters[t.second] = encounters.get(t.second,
+                                          (base64.b64encode(str(round(time.time()))),
+                                           random.randint(1, 150)))
+
+    return {
+        'encounterId': encounters.get(t.second)[0],
+        'id': encounters.get(t.second)[1],
+        'geo': {
+            'lat': round(random.uniform(48.8438105, 48.8883955), 6),
+            'lon': round(random.uniform(2.2635173, 2.3857733), 6)
+        },
+        'expireAt': (datetime.now() + timedelta(minutes=cycle_size, seconds=10)).isoformat(),
+    }
 
 print('connecting to kafka')
 p = Producer(bootstrap_servers='0.0.0.0',
              key_serializer=lambda x: str(x).encode(),
              value_serializer=json.dumps)
 print('connected to kafka')
-i = 0
 while True:
-    pokemon = random.choice(pokemons.items())
+    pokemon = scan()
     print('sending {}'.format(pokemon))
-    message = {
-        'id': pokemon[0],
-        'name': pokemon[1],
-        'geo': {
-            'lat': round(random.uniform(48.8191955, 48.9045735), 6),
-            'lon': round(random.uniform(2.2403523, 2.4028253), 6)
-        },
-        'expireAt': (datetime.now() + timedelta(minutes=5)).isoformat()
-    }
-    p.send('pokemons', key=pokemon[0], value=message)
+    p.send('pokemons', key=pokemon['encounterId'], value=pokemon)
     p.flush()
-    i += 1
-    time.sleep(1)
+    time.sleep(0.5)
 print('The End.')
