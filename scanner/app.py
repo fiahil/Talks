e83fec23dece4eb99c83ec0433a848adbab189fb
Fiahil <fiahil@gmail.com>
2016-09-11 22:33:01 +0200

    squash! minor adjustments to kafka messaging

--- scanner/app.py
+++ scanner/app.py
@@ -10,7 +10,7 @@ import random
 import base64
 import logging
 
-logging.basicConfig(format='%(asctime)s [%(levelname)8s] %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s [%(levelname)8s] %(message)s', level=logging.INFO)
 log = logging.getLogger('Scanner')
 
 CYCLE_SPEED = 0.5
@@ -27,19 +27,29 @@ def get_random_point(ref=None, step=0):
         round(random.uniform(2.2635173, 2.3857733), 6)
     )
 
-class Spawnpoint(namedtuple('Spawnpoint', ['pokemon_ids', 'ttl', 'frequency'])):
+
+class Pokemon(namedtuple('Pokemon', ['id', 'pos'])):
+    pass
+
+
+class Spawnpoint(namedtuple('Spawnpoint', ['spawn_id', 'pokemon_ids', 'ttl', 'frequency'])):
     def cycle(self, ref, i):
         if i % self.frequency == 0:
             # Create a new pokemon around the spawnpoint
-            log.debug('spawning pokemons')
+            log.info('spawning pokemons')
             pos = get_random_point(ref, 0.0001)
             id = random.choice(list(self.pokemon_ids))
-            log.debug('Spawning {} at {}'.format(id, pos))
-            return (id, pos)
+            p = Pokemon(id, pos)
+            log.info('spawned {}'.format(p))
+            return (self.spawn_id, p)
+        return (None, None)
+
 
 class Spawner:
-    def __init__(self):
+    def __init__(self, kafka):
         self.timer = 0
+        self.kafka = kafka
+        """kafka client"""
         self.spawnpoints = {}
         """{ (lat, lon): Spawnpoint(pokemon_ids, ttl, frequency) }"""
 
@@ -47,24 +57,27 @@ class Spawner:
         """Create new spawnpoints and delete expired ones
         """
         for p, s in self.spawnpoints.items():
-            ids, ttl, fcy = s
+            id, ids, ttl, fcy = s
             if ttl <= 0:
                 # Remove expired spawn
                 log.info("Expired spawnpoint @ {}".format(p))
+                kafka.send('spawnpoints', key='delete', value=id)
                 del self.spawnpoints[p]
             else:
-                self.spawnpoints[p] = Spawnpoint(ids, ttl - 1, fcy)
+                self.spawnpoints[p] = Spawnpoint(id, ids, ttl - 1, fcy)
 
         if self.timer <= 0:
             # Time to create a new spawnpoint
             point = get_random_point()
             spawnpoint = Spawnpoint(
+                base64.b64encode(str(time.time())), # SpawnpointId
                 {random.randint(1, 150) for i in range(random.randint(1, 3))},
                                           # Random pokemon ids
                 random.randint(30, 200),  # Random ttl
-                random.randint(1, 10)     # Random frequency
+                random.randint(10, 20)    # Random frequency
             )
             log.info('New spawnpoint with {} @ {}'.format(spawnpoint, point))
+            kafka.send('spawnpoints', key='create', value=spawnpoint.spawn_id)
             self.spawnpoints[point] = spawnpoint
 
             # Generate a new seed
@@ -76,55 +89,34 @@ class Spawner:
         """
         return [(p, s) for p, s in self.spawnpoints.items() if s.ttl > 0]
 
-def scan():
-    global encounters
-    t = datetime.now()
-
-    if t.second == 0 and t.minute % cycle_size:
-        encounters = {}
-
-    encounters[t.second] = encounters.get(t.second,
-                                          (base64.b64encode(str(round(time.time()))),
-                                           random.randint(1, 150)))
-
-    return {
-        'encounterId': encounters.get(t.second)[0],
-        'id': encounters.get(t.second)[1],
-        'geo': {
-            'lat': round(random.uniform(48.8438105, 48.8883955), 6),
-            'lon': round(random.uniform(2.2635173, 2.3857733), 6)
-        },
-        'expireAt': (datetime.now() + timedelta(minutes=cycle_size, seconds=10)).isoformat(),
-    }
 
 log.info('connecting to kafka')
-kafka = Producer(bootstrap_servers='0.0.0.0',
+kafka = Producer(bootstrap_servers='127.0.0.1',
                  key_serializer=lambda x: str(x).encode(),
                  value_serializer=json.dumps)
 log.info('connected to kafka')
 
-spawner = Spawner()
+spawner = Spawner(kafka)
 i = 0
 
 while True:
     log.info('> {}'.format(i))
     spawner.cycle()
-    log.debug('# activated spawners:')
+    log.info('# activated spawners:')
     for p, s in spawner.activated():
-        log.debug('## {}'.format(s))
-        p = s.cycle(p, i)
-        if p:
+        log.info('## {}'.format(s))
+        spawn_id, pokemon = s.cycle(p, i)
+        if spawn_id and pokemon:
             p = {
-                'encounterId': 'test',
-                'id': p[0],
+                'id': pokemon.id,
                 'geo': {
-                    'lat': p[1][0],
-                    'lon': p[1][1]
+                    'lat': pokemon.pos[0],
+                    'lon': pokemon.pos[1]
                 },
                 'expireAt': (datetime.now() + timedelta(minutes=5)).isoformat(),
             }
-            kafka.send('pokemons', key='test', value=p)
-            kafka.flush()
-    log.debug('# timer until new spawnpoint: {}'.format(spawner.timer))
+            kafka.send('pokemons', key=spawn_id, value=p)
+    log.info('# timer until new spawnpoint: {}'.format(spawner.timer))
+    kafka.flush()
     time.sleep(CYCLE_SPEED)
     i += 1
